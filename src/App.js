e470dcfa827d3ed2d054e0369c4e80fb1d634c63
chavierto <hola@xavierlujan.com>
2021-11-29 15:20:37 -0500

    Minor text changes

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ function App() {
 	return (
 		<div className='App'>
 			<header className='App-header'>
-				<h1>Patricia Garcia-Luján</h1>
+				<h1>Patricia García Luján</h1>
 			</header>
-			<p>Welcome to my site. I'm a writer based in Miami, Fl.</p>
-      <h2>About</h2>
-      <h2>Contact</h2>
-      <h2>Selected Writing</h2>
+			<h2>About</h2>
+			<p>Patricia García Luján is a writer based in Miami, Fl.</p>
+			<h2>Contact</h2>
+			<h2>Selected Writing</h2>
 		</div>
 	);
 }
